9f45b8b0a819ce38832d0e71adfbdaa5e2d199cc
raphaelsoul <raphaelsoul@163.com>
2015-04-14 14:56:33 +0900

    learned templates

--- app/views.py
+++ app/views.py
@@ -1,8 +1,24 @@
 __author__ = 'scb53'
 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "hello, here is index!"
+	user = { 'nickname': 'Miguel' }
+	posts = [ # fake array of posts
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+	return render_template("index.html",
+        title = 'Home',
+        user = user,
+		posts=posts
+		)
